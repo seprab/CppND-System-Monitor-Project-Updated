5fde6d1279a0d7593d19538cc65717e2690443db
sergio prada <sergioeprada@hotmail.com>
2024-04-30 22:55:15 -0500

    implemented ElapsedTime method

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,21 @@
 #include <string>
-
+#include <iomanip>
+#include <sstream>
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) 
+{
+    long hours = seconds / 3600;
+    seconds %= 3600;
+    long minutes = seconds / 60;
+    seconds %= 60;
+
+    std::ostringstream timeStream;
+    timeStream << std::setw(2) << std::setfill('0') << hours << ":"
+               << std::setw(2) << std::setfill('0') << minutes << ":"
+               << std::setw(2) << std::setfill('0') << seconds;
+
+    return timeStream.str();
+}
